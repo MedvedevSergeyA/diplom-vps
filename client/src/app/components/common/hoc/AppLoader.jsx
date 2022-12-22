98a7a157e2559e121a642f3a9f801b12134b0e8b
Sergey <medvedev.sergeyaa@gmail.com>
2022-12-23 04:28:25 +0500

    fix bag auth

--- client/src/app/components/common/hoc/AppLoader.jsx
+++ client/src/app/components/common/hoc/AppLoader.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadDeviceList } from "../../../store/deviceSlice";
+import { getDeviceLoadingStatus, loadDeviceList } from "../../../store/deviceSlice";
 import PropTypes from "prop-types";
 import Loader from "../Loader/loader";
-import { getUsersLoadingStatus, loadUsersList } from "../../../store/userSlice";
+import { getCurrentUserId, loadUsersList } from "../../../store/userSlice";
 
 const AppLoader = ({ children }) => {
   const dispatch = useDispatch();
-  const usersLoading = useSelector(getUsersLoadingStatus());
+  const deviceStatus = useSelector(getDeviceLoadingStatus())
+  const currentUserId = useSelector(getCurrentUserId)
 
   useEffect(() => {
     dispatch(loadDeviceList());
     dispatch(loadUsersList());
-  }, []);
+  }, [currentUserId]);
 
-  if (usersLoading) {
+  if (deviceStatus) {
     return <Loader />;
   }
   return children;
